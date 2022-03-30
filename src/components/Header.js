c9e64f2101cf1de2fc4fba937f882d0429056c3e
Gabriel Guiet-Dupré <gabriel.guiet-dupre@epitech.eu>
2022-03-30 19:28:30 +0200

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <nav>
+        <ul>
+          <NavLink
+            to="/"
+            className={(nav) => (nav.isActive ? "nav-active" : "")}
+          >
+            <li>Accueil</li>
+          </NavLink>
+          <NavLink
+            to="/coup-de-coeur"
+            className={(nav) => (nav.isActive ? "nav-active" : "")}
+          >
+            <li>Favoris</li>
+          </NavLink>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
